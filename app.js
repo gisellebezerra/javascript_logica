71357fb598d05c265f7d0dd80e39c51d6027e4f8
Giselle Bezerra <135065873+gisellebezerra@users.noreply.github.com>
2024-02-02 10:50:09 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,44 +1,21 @@
-//1º - Mostre um alerta com a mensagem "Boas boas-vindas ao nosso site".
+let milhas = prompt('Digite a quantidade de milhas que você possui');
 
-alert('Seja bem-vindo(a) ao site!');
 
-//2ª - Declare uma variável chamada nome e atribua a ela o valor "Lua".
+if (30000 >= milhas && milhas >= 5000) {
+    let passagem = prompt('Digite o valor da passagem para receber o desconto');
+    let desconto = passagem - (passagem * 0.10);
+    alert(`Seu desconto é de 10% e o valor final da passagem é de R$ ${desconto} reais`);
+} 
 
-let nome = 'Lua';
-
-//3ª - Crie uma variável chamada idade e atribua a ela o valor 25.
-
-let idade = 25;
-
-//4ª - Defina uma variável numeroDeVendas e atribua a ela o valor 50.
-
-let numeroDeVendas = 50;
-
-//5ª - Defina uma variável saldoDisponivel e atribua a ela o valor 1000.
-
-let saldoDisponivel =  1000;
-
-//6ª - Exiba um alerta com o texto "Erro! Preencha todos os campos.".
-
-alert('Erro! Preencha todos os campos.');
-
-//7ª - Declare uma variável chamada mensagemDeErro e atribua a ela o valor "Erro! Preencha todos os campos." Agora exiba um alerta com o valor da variável mensagemDeErro.
-
-let mensagemDeErro = 'Erro! Preencha todos os campos.';
-alert(mensagemDeErro);
-
-//8ª - Use um prompt para perguntar o nome do usuário e armazená-lo na variável nome.
-
-let nomeUsuario = prompt('Digite seu nome:');
-
-//9ª - Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade.
-
-let idadeUsuario = prompt('Digite sua idade:');
-
-//10ª - Agora, caso a idade seja maior ou igual a 18, exiba um alerta com a mensagem "Pode tirar a habilitação!".
-
-let idadeUsuarios = prompt('Digite sua idade:');
-
-if (idadeUsuario >= 18) {
-alert('Pode tirar a habilitação!');
+else if (30000 < milhas) {
+    let passagem = prompt('Digite o valor da passagem para receber o desconto');
+    let desconto = passagem - (passagem * 0.20);
+    alert(`Seu desconto é de 20% e o valor final da passagem é de R$ ${desconto} reais`);
 }
+
+else {
+    alert(`Sinto muito, você precisa acumular no mínimo 5000 milhas para ganhar desconto na sua passagem!`);
+} 
+
+
+
