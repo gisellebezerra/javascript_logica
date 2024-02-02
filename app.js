82174ebd25f7c0180d685bf0fa8adec037c6abeb
Giselle Bezerra <135065873+gisellebezerra@users.noreply.github.com>
2024-02-02 12:09:36 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+let idade = prompt('Digite sua idade');
+
+if (idade >= 18) {
+    alert('Parabéns, você já possui idade para retirar sua habilitação!');
+}
+
+else{
+    alert('Você não possui idade para retirar sua habilitação!')
+}
